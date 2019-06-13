160b99d271275e13808da3ae3350998dd43d6296
David Colmenero Guerra <6810709@renfeviajeros365.es>
2019-06-13 16:37:19 +0200

    first commit

---
+++ src/pl02/Simulador.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pl02;
+
+/**
+ * @author 6810709
+ *
+ */
+public class Simulador {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
