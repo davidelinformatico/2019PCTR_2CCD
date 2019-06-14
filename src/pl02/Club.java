016e48a915135afbad63d6630a98b84e32db3c1c
David Colmenero Guerra <6810709@renfeviajeros365.es>
2019-06-14 12:56:42 +0200

    Creación de monitor

--- src/pl02/Club.java
+++ src/pl02/Club.java
@@ -30,18 +30,50 @@ public class Club {
 		this.palosDisponibles = palosDisponibles;
 	}
 	
-	
-	public void reservar(int pelotas, int palos) {
+	/**
+	 * El proceso de reserva gestiona la entrega de material al jugador,
+	 * solo un jugador a la vez es atendido (excl. mutua).
+	 * 
+	 * Si no hay suficientes pelotas o palos, el jugador ha de esperar.
+	 * 
+	 * @param pelotas
+	 * @param palos
+	 */
+	public synchronized void reservar(int pelotas, int palos) {
+		while (pelotasDisponibles<pelotas || palosDisponibles<palos) {
+			// hacemos esperar al proceso hasta que haya material
+			try {
+				System.out.println("No hay material: " + this);
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		pelotasDisponibles -= pelotas;
 		palosDisponibles -= palos;
 	}
 	
 	
-	public void devolver(int pelotas, int palos) {
+	/**
+	 * La devolución de material también debe realizarse de modo que un
+	 * único jugador entre en el método.
+	 * Como la devolución puede hacerse siempre, independientemente de la
+	 * cantidad de material que haya, no se produce ninguna espera.
+	 * @param pelotas
+	 * @param palos
+	 */
+	public synchronized void devolver(int pelotas, int palos) {
 		pelotasDisponibles += pelotas;
 		palosDisponibles += palos;
-		
+		notifyAll();
 	}
+
+	@Override
+	public String toString() {
+		return "Club [pelotasDisponibles=" + pelotasDisponibles + ", palosDisponibles=" + palosDisponibles + "]";
+	}
+	
 	
 	
 }
